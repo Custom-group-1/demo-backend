e69ec746b7b9657607c8deea81e2fff4935770c4
Revdovo <63352181+Revdovo@users.noreply.github.com>
2026-01-13 18:34:30 +0700

    Fix mapping

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import type { Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('ip')
+  getIp(@Req() request: Request): string {
+    return request.ip || (request as any).connection?.remoteAddress || 'unknown';
   }
 }
